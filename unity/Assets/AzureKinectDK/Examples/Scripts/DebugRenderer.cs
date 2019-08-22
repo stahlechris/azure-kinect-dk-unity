dd540ac0735fb1be01c940310cfc046c40a574d4
Hololens Dev Pc <bitter.daniel@gmail.com>
2019-08-22 08:02:11 -0500

    Screencaps work

--- unity/Assets/AzureKinectDK/Examples/Scripts/DebugRenderer.cs
+++ unity/Assets/AzureKinectDK/Examples/Scripts/DebugRenderer.cs
@@ -55,57 +55,58 @@ public class DebugRenderer : Singleton<DebugRenderer>
 
     void Update()
     {
-        using (Capture capture = device.GetCapture())
-        {
-            tracker.EnqueueCapture(capture);
-            var color = capture.Color;
-            if (color.WidthPixels > 0)
-            {
-                Texture2D tex = new Texture2D(color.WidthPixels, color.HeightPixels, TextureFormat.BGRA32, false);
-                tex.LoadRawTextureData(color.GetBufferCopy());
-                tex.Apply();
-                renderer.material.mainTexture = tex;
-            }
-        }
-        
-        using (var frame = tracker.PopResult())
-        {
-            //Debug.LogFormat("{0} bodies found.", frame.NumBodies);
-            if (frame.NumBodies > 0)
-            {
-                var bodyId = frame.GetBodyId(0);
-                //Debug.LogFormat("bodyId={0}", bodyId);
-                this.skeleton = frame.GetSkeleton(0);
-                skeletons.Add(this.skeleton);
-                for (var i = 0; i < (int)JointId.Count; i++)
-                {
-                    var joint = this.skeleton.Joints[i];
-                    var pos = joint.Position;
-					Debug.Log("pos: " + (JointId)i + " " + pos[0] + " " + pos[1] + " " + pos[2]); // Length 3
-					//foreach (float t in pos)
-					//{
-					//	Debug.Log(t + "" + (JointId)i); //ex. rcvd: -484.9375EarLeft...6decimal pts rcvd...todo cap float
-					//}
+		using (Capture capture = device.GetCapture())
+		{
+			tracker.EnqueueCapture(capture);
+			var color = capture.Color;
+			if (color.WidthPixels > 0)
+			{
+				Texture2D tex = new Texture2D(color.WidthPixels, color.HeightPixels, TextureFormat.BGRA32, false);
+				tex.LoadRawTextureData(color.GetBufferCopy());
+				tex.Apply();
+				renderer.material.mainTexture = tex;
+			}
+		}
 
-                    var rot = joint.Orientation;
+		using (var frame = tracker.PopResult())
+		{
+			//Debug.LogFormat("{0} bodies found.", frame.NumBodies);
+			if (frame.NumBodies > 0)
+			{
+				var bodyId = frame.GetBodyId(0);
+				//Debug.LogFormat("bodyId={0}", bodyId);
+				this.skeleton = frame.GetSkeleton(0);
+				skeletons.Add(this.skeleton);
+				for (var i = 0; i < (int)JointId.Count; i++)
+				{
+					var joint = this.skeleton.Joints[i];
+					var pos = joint.Position;
+					Debug.Log("pos: " + (JointId)i + " " + pos[0] + " " + pos[1] + " " + pos[2]); // Length 3
+																								  //foreach (float t in pos)
+																								  //{
+																								  //	Debug.Log(t + "" + (JointId)i); //ex. rcvd: -484.9375EarLeft...6decimal pts rcvd...todo cap float
+																								  //}
+
+					var rot = joint.Orientation;
 					Debug.Log("rot " + (JointId)i + " " + rot[0] + " " + rot[1] + " " + rot[2] + " " + rot[3]); // Length 4
 					var v = new Vector3(pos[0], -pos[1], pos[2]) * 0.004f;
-                    var r = new Quaternion(rot[1], rot[2], rot[3], rot[0]);
-                    var obj = debugObjects[i];
-                    obj.transform.SetPositionAndRotation(v, r);
+					var r = new Quaternion(rot[1], rot[2], rot[3], rot[0]);
+					var obj = debugObjects[i];
+					obj.transform.SetPositionAndRotation(v, r);
 
-                    if (skeletons.Count > 50)
-                    {
-                        Debug.Log("we have enough skeletons");
+					if (skeletons.Count > 50)
+					{
+						Debug.Log("we have enough skeletons");
+						ScreenCapture.CaptureScreenshot("BlockMan-"); // + DateTime.Now.ToString().Replace(@"/", "."));
 						Debug.Log(System.DateTime.Now);
 						//this.device.StopCameras();
 						//NativeMethods.k4a_device_stop_cameras(handle);
 						//this.device.Dispose();
 						Debug.Break();
-                    }
-                }
-                
-            }
+					}
+				}
+
+			}
 		}
-    }
+	}
 }
